f43fd13c8e5782aa53ca4edad69c43b5b824a9d4
yazanQwaider <yazeno15@gmail.com>
2022-07-26 21:58:49 +0300

    Open specific chat for friends

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const {Router} = require('express');
 const router = Router();
 
 const peopleController = require('../src/controllers/peopleController')
+const chatController = require('../src/controllers/chatController');
 const authMiddleware = require('../src/middleware/authMiddleware');
 
 router.use('/api', authMiddleware.checkApiAuthentication);
@@ -10,6 +11,8 @@ router.use('/api', authMiddleware.checkApiAuthentication);
 
 router.get('/api/people', peopleController.api_people_get);
 router.post('/api/people/:required_action/:id', peopleController.api_people_post);
+
+router.get('/api/chats/:user_id', chatController.api_get_messages);
 
 
 module.exports = router;
--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,3 +1,57 @@
-module.exports.chats_get = function(req, res) {
-    res.render('chats');
+module.exports.chats_get = async function(req, res) {
+    const ObjectId = require('mongodb').ObjectId;
+    let friendsIds = [];
+    if(req.session.user.friends) {
+        req.session.user.friends.forEach(function(friendId) {
+            friendsIds.push(new ObjectId(friendId));
+        })
+    }
+
+    const mongodb = require('../connectDB');
+    let database = mongodb().db('chat_db');
+    let users = database.collection('users');
+    let friends = await users.find({_id: {$in: friendsIds}}).toArray();
+
+    res.render('chats', {friends: friends});
+}
+
+
+module.exports.api_get_messages = async function(req, res) {
+    let authUser = req.session.user;
+    let friendUserId = req.params.user_id;
+
+    const mongodb = require('../connectDB');
+    let database = mongodb().db('chat_db');
+    let chats = database.collection('chats');
+    let ObjectId = require('mongodb').ObjectId;
+    
+    let users = database.collection('users');
+    authUser = await users.findOne({_id: new ObjectId(authUser._id) });
+    let authUserChats = authUser.chats;
+
+    let chat = null;
+    if(authUserChats && authUserChats.findIndex((chatItem) => new ObjectId(friendUserId).equals(chatItem._id)) != -1 ) {
+        let chatIndex = authUserChats.findIndex((chatItem) => new ObjectId(friendUserId).equals(chatItem._id));
+        chat = await chats.findOne({_id: new ObjectId(authUserChats[chatIndex].chatId) });
+    }
+    else {
+        let createdChat = await chats.insertOne({
+            user_1: new ObjectId(authUser._id),
+            user_2: new ObjectId(friendUserId), 
+            last_message: null,
+        });
+
+        let userChat = {_id: new ObjectId(friendUserId), chatId: createdChat.insertedId};
+        users.updateOne(
+            {_id: new ObjectId(authUser._id)}, 
+            {$push: {chats: userChat}}
+        );
+
+        chat = await chats.findOne({_id: createdChat.insertedId});
+        req.session.save(function() {
+            req.session.user = authUser
+        });
+    }
+
+    res.json({chat: chat});
 }
