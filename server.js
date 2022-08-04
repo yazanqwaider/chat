4e5e45e4aab49376a3780f70134e09ba98895f34
yazanQwaider <yazeno15@gmail.com>
2022-08-04 20:45:28 +0300

    Activate socket.io in chats, by emit the messages

--- server.js
+++ server.js
@@ -26,11 +26,27 @@ const apiRoutes = require('./routes/api');
 app.use(webRoutes);
 app.use(apiRoutes);
 
-
-app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
-})
-
-
 const server = http.createServer(app);
 const io = new Server(server);
+
+
+let users = new Map();
+io.on('connection', socket => {
+    socket.on('connected', function(userId) {
+        users[userId] = socket.id;
+    });
+
+    socket.on('messageSended', function(messageObject) {
+        if(users[messageObject.receiver_id]) {
+            socket.to(users[messageObject.receiver_id]).emit('messageReceived', JSON.stringify(messageObject));
+        }
+    });
+
+    socket.on('disconnect', (reason) => {
+        console.log('connected closed');
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+})
