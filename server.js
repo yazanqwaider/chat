934f04d1c3e0e4f19d125de32a104746b99edad1
yazanQwadier <yazeno15@gmail.com>
2022-08-11 08:47:59 +0300

    Install & initialize tailwindcss on project

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 
 app.use(session({
