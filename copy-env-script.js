b57c53f0e2786044f3c1bc45ef681b335fc05ba2
yazanQwadier <yazeno15@gmail.com>
2022-09-07 14:51:56 +0300

    Add build command for postcss

--- copy-env-script.js
+++ copy-env-script.js
@@ -8,7 +8,7 @@ fs.readFile('.env', 'utf8', (err, data) => {
     let env_content = data;
     
     const key = 'ACCESS_TOKEN_SECRET';
-    const new_token = crypto.randomBytes(32).toString('base64');
+    const new_token = crypto.createHash('sha256').update(crypto.randomBytes(32)).digest('base64');
     const old_value = key + '=' + process.env[key];
     const new_value = key + '=' + new_token;
 
